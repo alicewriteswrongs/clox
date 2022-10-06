abe6db531abe6e4b55ae59be613849df59042fdd
Alice Pote <alice.writes.wrongs@gmail.com>
2022-10-06 16:21:12 -0400

    got a formatter set up

--- src/chunk.h
+++ src/chunk.h
@@ -4,16 +4,16 @@
 #include "common.h"
 
 typedef enum {
-    OP_RETURN,
+  OP_RETURN,
 } OpCode;
 
 typedef struct {
-    int count;
-    int capacity;
-    uint8_t* code;
+  int count;
+  int capacity;
+  uint8_t *code;
 } Chunk;
 
-void initChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void initChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte);
 
 #endif
